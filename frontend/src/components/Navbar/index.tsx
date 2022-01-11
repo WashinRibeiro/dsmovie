4b7e1928e76a4d714ead9c959522a57124c119d6
Washington Ribeiro da Silva <washinuchiha@gmail.com>
2022-01-11 01:18:17 -0300

    Pagination layout

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,23 +1,23 @@
-import {ReactComponent as GithubIcon} from 'assets/img/github.svg';
+import { ReactComponent as GithubIcon } from 'assets/img/github.svg';
 import './styles.css';
 
 function Navbar() {
 
-    return(
+  return (
     <header>
       <nav className="container">
         <div className="dsmovie-nav-content">
           <h1>DSMovie</h1>
           <a href="https://github.com/WashinRibeiro/" target="_blank" rel="noreferrer">
-          <div className="dsmovie-contact-container">
-            <GithubIcon />
-            <p className="dsmovie-contact-link">/washinRibeiro</p>
-          </div>
+            <div className="dsmovie-contact-container">
+              <GithubIcon />
+              <p className="dsmovie-contact-link">/washinRibeiro</p>
+            </div>
           </a>
         </div>
       </nav>
     </header>
-    );
+  );
 
 }
 
